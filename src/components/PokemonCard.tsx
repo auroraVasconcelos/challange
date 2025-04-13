d81256df1dd08756b3e9075e995f42752b407aff
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2025-04-13 19:07:57 +0100

    feat: add id number to cards; fix keys issue

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -13,6 +13,7 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, image, types }) => 
   return (
     <Link href={`/pokemon/${id}`}>
       <div className="p-4 border rounded-lg shadow hover:scale-105 transition-transform flex flex-col items-center cursor-pointer bg-white">
+        <div className="text-xs text-gray-500 font-mono w-full flex justify-end">#{id.toString().padStart(3, "0")}</div>
         <img src={image} alt={name} className="w-24 h-24 object-contain" />
         <p className="capitalize font-semibold mt-2 flex text-center">{name}
           {types.map((type) => (
